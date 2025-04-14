41b546af485e0b2fb0941d159a25dead01ad3f97
calvinoke <115186372+calvinoke@users.noreply.github.com>
2025-04-14 22:21:21 +0300

    Delete src/main/java/com/example/backend directory

--- src/main/java/com/example/backend/exception/TeacherNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.exception;
-
-public class TeacherNotFoundException extends RuntimeException {
-    public TeacherNotFoundException(String message) {
-        super(message);
-    }
-}
